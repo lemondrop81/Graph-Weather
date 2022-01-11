32966d5f30760b93e785d310b67cebe0b9aa30b3
lemondrop81 <hacker814@hotmail.com>
2022-01-11 11:01:41 -0600

    Fetching weather data

--- weather.py
+++ weather.py
@@ -1,3 +1,9 @@
 from requests import get
 import matplotlib.pyplot as plt
 from dateutil import parser
+
+# Web api to get the weather at the weather station
+url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallmeasurements/552355'
+
+# save the data to a variable
+weather = get(url).json()
