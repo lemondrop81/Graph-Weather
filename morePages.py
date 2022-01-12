a88234f4d857ef00b07df1764e01280a45ebfd33
lemondrop81 <hacker814@hotmail.com>
2022-01-12 15:53:52 -0600

    Add comments regarding assigning variables

--- morePages.py
+++ morePages.py
@@ -8,15 +8,23 @@ pages = 1
 weather = get(url).json()
 data = weather['items']
 
-temperatures = [record['ambient_temp'] for record in data]
+# Assigns the timestamp data set to a variable
 timestamps = [parser.parse(record['reading_timestamp']) for record in data]
 
+# Assigns the ambient temperature data set to a value
+temperatures = [record['ambient_temp'] for record in data]
+
+# Assigns the air quality data set to a variable
 airQuality = [record['air_quality'] for record in data]
 
+# Assigns the humidity data set to a variable
 humidity = [record['humidity'] for record in data]
 
+# Plots the data sets 
 plt.plot(timestamps, airQuality, label='Air Quality Index')
 plt.plot(timestamps, temperatures, label='temp °c')
 plt.plot(timestamps, humidity, label='humidity %')
+
+# Display the label and legends
 plt.legend()
 plt.show()
