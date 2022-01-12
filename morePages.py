23a1848c304209b54e392a84b6128fa46ab21f90
lemondrop81 <hacker814@hotmail.com>
2022-01-12 15:30:18 -0600

    Plot air quality

--- morePages.py
+++ morePages.py
@@ -8,17 +8,12 @@ pages = 1
 weather = get(url).json()
 data = weather['items']
 
-while 'next' in weather and pages < 9:
-    url = weather['next']['$ref']
-    print('Fetching {0}'.format(url))
-    weather = get(url).json()
-    data += weather['items']
-    pages += 1
-
 temperatures = [record['ambient_temp'] for record in data]
 timestamps = [parser.parse(record['reading_timestamp']) for record in data]
 
-plt.plot(timestamps, temperatures)
+airQuality = [record['air_quality'] for record in data]
+
+plt.plot(timestamps, airQuality)
 plt.ylabel('Temperature')
 plt.xlabel('date and time')
 plt.show()
